8eb6e1a070a70e142ba4a329dcfe6053fbb9581a
Kalen Willits <kalenwillits@gmail.com>
2023-05-17 00:12:07 -0400

    Logic to check for already added fn to bash script.

--- run/install.sh
+++ run/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set -xe;
-cargo build --release;
-mkdir -p ~/Apps/fn/;
-cp target/release/fn ~/Apps/fn/fn;
-echo "export PATH=\$PATH:~/Apps/fn/" >> ~/.bashrc;
-echo "Insallation complete! Please restart your shell session";
+set -xe
+cargo build --release
+mkdir -p ~/Apps/fn/
+cp target/release/fn ~/Apps/fn/fn
+content="export PATH=\$PATH:~/Apps/fn/" 
+if grep -Fxq $content ~/.bashrc 
+then
+echo $content >> ~/.bashrc
+fi
+echo "Insallation complete! Please restart your shell session..."
