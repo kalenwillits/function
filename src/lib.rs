9c0d7e73f313e71114234579b9a578aab1898ed5
Kalen Willits <kalenwillits@gmail.com>
2023-05-16 23:45:31 -0400

    Removing uneeded println!

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
 #[cfg(test)]
 mod tests;
 
+use home;
+use std::collections::HashMap;
 use std::env;
-use std::process::Command;
 use std::ffi::OsString;
 use std::fs;
-use std::path::PathBuf;
-use std::collections::HashMap;
 use std::io::{self, Write};
-use home;
+use std::path::PathBuf;
+use std::process::Command;
 
 fn load_functions(cache: &mut HashMap<OsString, OsString>, dir: &mut PathBuf) -> Result<(), String> {
     dir.push("run");
-    for function in fs::read_dir(dir).expect("Unable to read dir") {
-        if let Ok(function_path) = function {
-            if let Some(function_name) = function_path.path().file_stem() {
-                cache.insert(function_name.to_os_string(), function_path.path().into_os_string());
-            }
+    if dir.is_dir() {
+        for function in fs::read_dir(dir).expect("Unable to read dir") {
+            if let Ok(function_path) = function {
+                if let Some(function_name) = function_path.path().file_stem() {
+                    cache.insert(function_name.to_os_string(), function_path.path().into_os_string());
+                }
 
-        };
+            };
+        }
     }
     Ok(())
 }
@@ -37,24 +39,27 @@ fn use_arg(cache: &HashMap<OsString, OsString>, key: OsString, args: &[String]) 
 
 pub fn run(args: Vec<String>) -> Result<(), String> {
     let mut cache: HashMap<OsString, OsString> = HashMap::new();
-    load_functions(&mut cache, &mut home::home_dir().expect("Unable to locate home dir"))?;
-    load_functions(&mut cache, &mut env::current_dir().expect("Unable to gather working dir"))?;
+    let _ = load_functions(&mut cache, &mut home::home_dir().expect("Unable to locate home dir"));
+    let _ = load_functions(&mut cache, &mut env::current_dir().expect("Unable to gather working dir"));
     if args.len() > 1 {
         if (args[1] == "-l") | (args[1] == "--list") {
             for (key, value) in cache {
-                println!("{}: {}", key.to_str().expect("Could not unwrap key"), value.to_str().expect("Could not unwrap value"));
+                println!(
+                    "{}: {}", 
+                    key.to_str()
+                        .expect("Could not unwrap key"), 
+                    value
+                        .to_str()
+                        .expect("Could not unwrap value"));
             }
-        } else if (args[1] == "-n") | (args[1] == "--new") {
-
         } else {
             let key = OsString::from(&args[1]);
-            println!("{:?}", &cache[&key]);
             if cache.contains_key(&key) {
                 use_arg(&cache, key, &args[2..]);
             }
         }
     } else {
-        return Err(String::from("No arguments supplied."));
+        return Err(String::from("No arguments supplied"));
     }
     Ok(())
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,34 @@
 use super::*;
 
+use std::collections::HashMap;
+use std::env;
+
+
+#[test]
+fn load_global_functions() -> Result<(), String> {
+    let mut cache: HashMap<OsString, OsString> = HashMap::new();
+    load_functions(&mut cache, &mut home::home_dir().expect("Failed to load home dir"))
+}
+
+
+#[test]
+fn load_local_functions() -> Result<(), String> {
+    let mut cache: HashMap<OsString, OsString> = HashMap::new();
+    load_functions(&mut cache, &mut env::current_dir().expect("Failed to load current dir"))
+}
+
+#[test]
+fn exec_list() -> Result<(), String> {
+    run(vec![String::from("/home/user/dummy"), String::from("--list")])
+}
+
+
+#[test]
+fn exec_missing() {
+    assert!(!run(vec![String::from("/home/user/dummy"), String::from("non-existant-function-1231231251")]).is_err())
+}
+
+
 
 
 
